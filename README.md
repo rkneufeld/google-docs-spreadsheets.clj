5fa1bd943deefe3fa8fec552dc7295cc6c409ef9
Ryan Neufeld <ryan@rkn.io>
2014-01-21 09:43:56 -0500

    Rely on Atlassian packaged version of gdata libs

---
+++ README.md
@@ -0,0 +1,22 @@
+google-docs-spreadsheets
+========================
+
+A Clojure library designed to interact with Google spreadsheets
+
+
+Usage
+-----
+
+Add the following to your dependencies:
+
+```clojure
+[io.rkn/google-docs-spreadsheets "0.2.0-SNAPSHOT"]
+```
+
+
+License
+-------
+
+Copyright © 2013 Mariano Guerra
+
+Distributed under the Eclipse Public License, the same as Clojure.
--- project.clj
+++ project.clj
@@ -1,22 +1,12 @@
-(defproject marianoguerra/google-docs-spreadsheets "0.1.1-SNAPSHOT"
-  :description "Library to interact with google spreadsheets"
-  :url "http://github.com/marianoguerra/google-docs-spreadsheets"
+(defproject rkneufeld/google-docs-spreadsheets "0.2.0-SNAPSHOT"
+  :description "Library to interact with Google spreadsheets"
+  :url "http://github.com/rkneufeld/google-docs-spreadsheets.clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :main marianoguerra.google-docs-spreadsheets-cli/main
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.cli "0.2.2"]
-
-                 [gdata-core/gdata-core "1.0"]
-                 [gdata-base/gdata-base "1.0"]
-                 [gdata-client/gdata-client "1.0"]
-                 [gdata-client-meta/gdata-client-meta "1.0"]
-                 [gdata-docs/gdata-docs "3.0"]
-                 [gdata-docs-meta/gdata-docs-meta "3.0"]
-                 [gdata-spreadsheet/gdata-spreadsheet "3.0"]
-                 [gdata-spreadsheet-meta/gdata-spreadsheet-meta "3.0"]
-
+                 [com.atlassian.bundles/gdata-java-client "1.41.4"]
                  [com.google.oauth-client/google-oauth-client "1.15.0-rc"]
                  [com.google.http-client/google-http-client "1.15.0-rc"]
-                 [com.google.guava/guava "11.0.2"]])
+                 [com.google.guava/guava "11.0.2"]]
+  :repositories [["atlassian" "https://maven.atlassian.com/content/groups/public/"]])
 
